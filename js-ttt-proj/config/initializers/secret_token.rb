75c1dd0b5fdc6134bfee4bc5aca906d06b8a5149
danielwells <danielwells.usnret@gmail.com>
2014-04-02 15:54:20 -0500

    Initial rails folder structure with users controller generated

---
+++ js-ttt-proj/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsTttProj::Application.config.secret_token = '762540828d966dcd82167dbe5c10db6b52b4f83378779b9b7a97fd8b372f0096be271c8f31d538830cdecfb8298ef4e098027d1c3fa670c4b05a28601c1ad19f'
